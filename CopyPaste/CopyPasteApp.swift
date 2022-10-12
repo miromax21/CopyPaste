121731373de136d336a9a1860f48b1c8f17ea022
maxim.mironov <maxim.mironov@cifrasoft.com>
2022-10-12 16:38:57 +0400

    Initial Commit

---
+++ CopyPaste/CopyPasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopyPasteApp.swift
+//  CopyPaste
+//
+//  Created by Sergey Zhidkov on 12.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CopyPasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
